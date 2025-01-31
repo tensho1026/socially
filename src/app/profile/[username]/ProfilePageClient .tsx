8fbed274b3856374c4cc52b5d7b0f9abcc92b6af
tensho1026 <tensho1026@icloud.com>
2025-01-31 23:53:42 +0900

    profileの途中まで

---
+++ src/app/profile/[username]/ProfilePageClient .tsx
@@ -0,0 +1,22 @@
+"use client"
+
+
+
+interface ProfilePageClientProps {
+  user:string,
+  post:string,
+  likedPosts:string,
+  isFollowing:boolean
+
+
+}
+
+
+
+function ProfilePageClient ({}:ProfilePageClientProps) {
+  return (
+    <div>ProfilePageClient </div>
+  )
+}
+
+export default ProfilePageClient 
---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,36 @@
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/pofile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient ";
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
+async function ProfilePageServer({ params }: { params:{ username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if(!user) notFound()
+
+    const [posts,likedPosts,isCurrentUserFollowing] = await Promise.all([
+      getUserPosts(user.id),
+      getUserLikedPosts(user.id),
+      isFollowing(user.id)
+
+    ])
+  return <ProfilePageClient 
+  user={user}
+  post={posts}
+  likedPosts={likedPosts}
+  isFollowing={isCurrentUserFollowing}
+  />;
+}
+
+export default ProfilePageServer;
